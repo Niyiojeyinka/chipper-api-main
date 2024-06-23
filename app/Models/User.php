e361fb0439dc5c5cc6e9661a82308a269ab6c10a
Olaniyi Ojeyinka <olaniyiojeyinka@gmail.com>
2024-06-24 00:41:44 +0100

    initialize project

---
+++ app/Models/User.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Favoritable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class User extends Authenticatable
+{
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
+    use Favoritable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function favoriteUsers()
+    {
+        return $this->belongsToMany(User::class, 'favorites', 'user_id', 'favoritable_id')
+                    ->wherePivot('favoritable_type', FavoritableType::USER->value);
+    }
+
+    public function favoritePosts()
+    {
+        return $this->belongsToMany(Post::class, 'favorites', 'user_id', 'favoritable_id')
+                    ->wherePivot('favoritable_type', FavoritableType::POST->value);
+    }
+}
